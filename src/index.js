7f614729601f6b8cffd0da31b9d22061e0c61d55
Paul_Beckwith <303dog@gmail.com>
2020-10-28 13:04:37 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function(){
+
+
+
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(){
+    event.preventDefault()
+    const input = document.getElementById("new-task-description")
+    const value = input.value
+    const li = document.createElement("li")
+    li.innerText = value
+    const ul = document.getElementById("tasks")
+    ul.append(li)
+    input.value = ""
+  })
+  
+
 });
